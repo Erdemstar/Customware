12e52385c8c5ce2a8e3b405e4b34cf394df80359
Erdemstar <erdem-yildiz@windowslive.com>
2023-04-08 18:29:13 +0300

    this is first commit but project is working well

---
+++ NetworkIO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customware
+{
+    internal class NetworkIO
+    {
+        public string GetHttpResponse(string url)
+        {
+            var client = new HttpClient();
+
+            try
+            {
+                var response = client.GetAsync(url).Result;
+                response.EnsureSuccessStatusCode();
+               
+                var responseBody = response.Content.ReadAsStringAsync().Result;
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                //Console.WriteLine($"HTTP hata oluştu. Mesaj: {e.Message}");
+                return null;
+            }
+        }
+    }
+}
